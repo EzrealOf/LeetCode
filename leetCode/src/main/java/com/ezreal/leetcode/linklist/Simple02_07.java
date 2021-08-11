85ba0e8899a5e1f9f53231ac3516e53330f2c2e0
ezrealok <ezrealok@163.com>
2021-08-11 08:57:58 +0800

    finish:2.7 init

--- leetCode/src/main/java/com/ezreal/leetcode/linklist/Simple02_07.java
+++ leetCode/src/main/java/com/ezreal/leetcode/linklist/Simple02_07.java
@@ -1,5 +1,7 @@
 package com.ezreal.leetcode.linklist;
 
+import static com.ezreal.leetcode.linklist.ListNode.createLinkList;
+
 /**
  * Created on 2021/8/8.
  * <p>
@@ -41,4 +43,86 @@ package com.ezreal.leetcode.linklist;
  * @since 2.0.0
  */
 public class Simple02_07 {
+
+    public static void main(String[] args) {
+        int[] numsA = {4, 1, 8, 4, 5};
+        int[] numsB = {5, 0, 1, 8, 4, 5};
+        ListNode headA = createLinkList(numsA);
+        ListNode headB = createLinkList(numsB);
+        System.out.println(headA);
+        System.out.println(headB);
+        ListNode listNode = getIntersectionNode2(headA, headB);
+        System.out.println(listNode);
+    }
+
+    public static ListNode getIntersectionNode(ListNode nodeA, ListNode nodeB) {
+        if (nodeA == null || nodeB == null) {
+            return null;
+        }
+        //calculate node lengths;
+        Integer nodeALength = calculateLength(nodeA);
+        Integer nodeBLength = calculateLength(nodeB);
+        return null;
+
+    }
+
+    public static Integer calculateLength(ListNode node) {
+        if (node == null)
+            return 0;
+        int count = 0;
+        while (node.next != null) {
+            count++;
+            node = node.next;
+        }
+        return count;
+    }
+
+    public static ListNode moveNode(ListNode node, Integer index) {
+        return null;
+
+    }
+
+    public static ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
+        ListNode curA = headA;
+        ListNode curB = headB;
+        int lenA = 0, lenB = 0;
+        while (curA != null) { // 求链表A的长度
+            lenA++;
+            curA = curA.next;
+        }
+        while (curB != null) { // 求链表B的长度
+            lenB++;
+            curB = curB.next;
+        }
+        curA = headA;
+        curB = headB;
+        // 让curA为最长链表的头，lenA为其长度
+        if (lenB > lenA) {
+            //1. swap (lenA, lenB);
+            int tmpLen = lenA;
+            lenA = lenB;
+            lenB = tmpLen;
+            //2. swap (curA, curB);
+            ListNode tmpNode = curA;
+            curA = curB;
+            curB = tmpNode;
+        }
+        // 求长度差
+        int gap = lenA - lenB;
+        // 让curA和curB在同一起点上（末尾位置对齐）
+        while (gap-- > 0) {
+            curA = curA.next;
+        }
+        // 遍历curA 和 curB，遇到相同则直接返回
+        while (curA != null) {
+            if (curA == curB) {
+                return curA;
+            }
+            curA = curA.next;
+            curB = curB.next;
+        }
+        return null;
+    }
+
+
 }
